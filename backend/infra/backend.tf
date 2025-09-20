702f8ddc7b8c6a9921e8cf029e9c446c33dacb40
Shyamm Sundar K <shyammsundar.k@idp.com>
2025-09-20 09:45:45 +0000

    removed backend

--- backend/infra/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket  = "testcase-generation-tf-bucket"
-    prefix  = "terraform/state"
-  }
-}
--- backend/infra/topic.tf
+++
@@ -1,3 +0,0 @@
-resource "google_pubsub_topic" "testcase_generation_issue_events" {
-  name = "testcase-generation-issue-events"
-}
