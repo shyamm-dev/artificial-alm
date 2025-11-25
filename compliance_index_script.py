c0b39dcbc9f24f3a541817c322ee376d728d38b0
avinashvcodes <avinashv.codes@gmail.com>
2025-11-25 18:26:27 +0530

    compliance index and reverse index creation

---
+++ compliance_index_script.py
@@ -0,0 +1,26 @@
+import json
+
+from collections import defaultdict
+
+base_path = "constants/"
+list_of_compliance_files = ["FDA.json", "HIPAA.json", "ISO-IEC-27001-2022.json"]
+
+index = {}
+reverse_index = defaultdict(list)
+
+for files in list_of_compliance_files:
+    with open(base_path + files, "r", encoding="utf-8") as compliance_file:
+        compliance_clauses = json.load(compliance_file)
+
+        for compliance_clause in compliance_clauses:
+            doc_id = compliance_clause["source"]+" "+compliance_clause["clause_id"]
+            index[doc_id] = compliance_clause
+
+            for tag in compliance_clause["tags"]:
+                reverse_index[tag].append(doc_id)
+
+with open(base_path + "compliance_index.json", "w", encoding="utf-8") as index_file:
+    json.dump(index, index_file, indent=4)
+
+with open(base_path + "compliance_reverse_index.json", "w", encoding="utf-8") as reverse_index_file:
+    json.dump(reverse_index, reverse_index_file, indent=4)
